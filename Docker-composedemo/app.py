805b31dfae14c0f95cfa4b7d9f5ede47f75f2687
soumya625 <51965945+soumya625@users.noreply.github.com>
2025-12-17 00:00:47 +0000

    Updated files

--- Docker-composedemo/app.py
+++ Docker-composedemo/app.py
@@ -1,8 +1,10 @@
-from flask import Flask
-app = Flask(_name_)
+from flask import Flask  # Import Flask
+app = Flask(__name__)  # Create app instance
 
 @app.route("/")
-def home():
-    return "Flask App running using Docker Compose!"
+def hello():
+    return "Hello from Dockerized Flask app!"  # This line is indented 4 spaces
 
-app.run(host="0.0.0.0", port=5000)
+if  __name__ == "__main__":
+    # Indented 4 spaces from the if statement
+    app.run(host="0.0.0.0", port=5000)
